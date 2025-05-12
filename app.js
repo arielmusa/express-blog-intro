c278df1be8f5d0aee73b79d3fc3c86bbf31ce093
arielmusa <arielmusainfo@gmail.com>
2025-05-12 16:23:29 +0200

    app.js: add dotenv and use env variables to handle host and port. Add array with blog posts. Add get route "/bacheca" to res.json(blogPosts)

--- app.js
+++ app.js
@@ -1,11 +1,43 @@
+import "dotenv/config";
+const { APP_HOST, APP_PORT } = process.env;
 import express from "express";
 const app = express();
-const port = 3000;
+
+const url = `${APP_HOST}${APP_PORT ? ":" + APP_PORT : ""}`;
+
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send("Server del mio blog");
 });
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+const blogPosts = [
+  {
+    title: "Ciambellone",
+    img_url: `${url}/images/ciambellone.jpeg`,
+  },
+  {
+    title: "Cracker barbabietola",
+    img_url: `${url}/images/cracker_barbabietola.jpeg`,
+  },
+  {
+    title: "Pane fritto dolce",
+    img_url: `${url}/images/pane_fritto_dolce.jpeg`,
+  },
+  {
+    title: "Pasta barbabietola",
+    img_url: `${url}/images/pasta_barbabietola.jpeg`,
+  },
+  {
+    title: "Torta paesana",
+    img_url: `${url}/images/torta_paesana.jpeg`,
+  },
+];
+
+app.get("/bacheca", (req, res) => {
+  res.json(blogPosts);
+});
+
+app.listen(APP_PORT, () => {
+  console.log(`Listening on ${url}`);
 });
